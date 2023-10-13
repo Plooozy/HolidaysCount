a6442bbaf512fb3f34b27410ac430a1dff30fdfc
unknown <lolipop22234@gmail.com>
2023-10-13 14:11:55 +0300

    Added .csv file and parameterized test

--- src/main/java/ru/netology/services/HolidaysCalcService.java
+++ src/main/java/ru/netology/services/HolidaysCalcService.java
@@ -1,4 +1,5 @@
 package ru.netology.services;
+
 public class HolidaysCalcService {
     public int calculate(int income, int expenses, int threshold) {
         int count = 0; // счётчик месяцев отдыха
--- src/test/java/ru/netology/services/HolidaysCalcServiceTest.java
+++ src/test/java/ru/netology/services/HolidaysCalcServiceTest.java
@@ -1,28 +1,14 @@
 package ru.netology.services;
+
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-//import ru.netology.services.HolydaysCalcService;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class HolidaysCalcServiceTest {
-    @Test
-    void shouldCalcMounths() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/Holiday.csv")
+    void shouldCalcMounths(int income, int expenses, int threshold, int expected) {
         HolidaysCalcService service = new HolidaysCalcService();
-        int income = 10_000; // Доход
-        int expenses = 3_000; // Траты в 1 месяц
-        int threshold = 20_000; // Сумма накопления с которой можно уйти в отпуск
-        int expected = 3; // Ожидаемое количество месяцев
-
-        // Вызываем метод
-        int months = service.calculate(income, expenses, threshold);
-        Assertions.assertEquals(expected, months);
-    }
-    @Test
-    void shouldCalcAnotherMounths() {
-        HolidaysCalcService service = new HolidaysCalcService();
-        int income = 100_000; // Доход
-        int expenses = 60_000; // Траты в 1 месяц
-        int threshold = 150_000; // Сумма накопления с которой можно уйти в отпуск
-        int expected = 2; // Ожидаемое количество месяцев
 
         // Вызываем метод
         int months = service.calculate(income, expenses, threshold);
